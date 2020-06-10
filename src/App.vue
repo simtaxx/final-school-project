6e06cfabbc8c1108f48706f55ea7ddfedc2753cf
simtaxx <souici.haris@gmail.com>
2020-06-10 14:52:02 +0200

    App - delete some useless things

--- src/App.vue
+++ src/App.vue
@@ -40,17 +40,7 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
-  name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+  name: "App"
 };
 </script>
