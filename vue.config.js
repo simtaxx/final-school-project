a82e2265266c7fc90bb3a979943d6113839043d4
simtaxx <souici.haris@gmail.com>
2020-06-10 14:54:25 +0200

    App - delete some useless things

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   lintOnSave: false,
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: '\n          @import "@/scss/styles.scss";\n        '
-      }
-    }
-  },
   transpileDependencies: ["vuetify"]
 };
