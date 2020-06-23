25bf5ea6a366ce210d65e48fad4985ba598d3368
simtaxx <souici.haris@gmail.com>
2020-06-23 17:08:11 +0200

    feat(nav): nav began

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,32 @@
-<template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <router-view :class="$style.router" />
-  </v-app>
+<template lang="pug">
+  v-app(:class="$style.app")
+    nav
+      div(:class="$style.profile")
+        h3 PROFIL
+        router-link(to="/sign-in" :class="$style.signUp") CONNEXION
+        router-link(to="/sign-up" :class="$style.signIn") INSCRIPTION
+      div(:class="$style.discover")
+        h3 DÉCOUVRIR
+        router-link(to="/")
+          img(src="@/assets/icons/institution.svg") 
+          p L'institution
+        router-link(to="/") 
+          img(src="@/assets/icons/citoyennete.svg") 
+          p La citoyenneté
+        router-link(to="/") 
+          img(src="@/assets/icons/justice.svg") 
+          p La justice
+        router-link(to="/") 
+          img(src="@/assets/icons/citoyennete.svg") 
+          p La citoyenneté
+        router-link(to="/") 
+          img(src="@/assets/icons/citoyennete.svg") 
+          p La citoyenneté
+        router-link(to="/") 
+          img(src="@/assets/icons/citoyennete.svg") 
+          p La citoyenneté
+      div(:class="$style.challenge")
+    router-view
 </template>
 
 <script>
@@ -43,7 +36,60 @@ export default {
 </script>
 
 <style lang="scss" module>
-.router {
-  margin-top: 56px;
+.app {
+  padding: 0 50px;
+}
+
+nav {
+  padding-top: 70px;
+}
+
+h3 {
+  font-family: "GothamMedium", sans-serif;
+  font-size: 12px;
+  color: #9498a0;
+  margin-bottom: 16px;
+}
+
+.profile {
+  display: flex;
+  flex-direction: column;
+
+  .signUp {
+    font-size: 12px;
+    font-family: "GothamMedium", sans-serif;
+    padding: 8px 16px;
+    background-color: #09bded;
+    width: 111px;
+    border-radius: 5px;
+    color: #faf9f9 !important;
+    margin-bottom: 8px;
+  }
+
+  .signIn {
+    font-size: 12px;
+    font-family: "GothamMedium", sans-serif;
+    padding: 8px 16px;
+    border: 1px solid #09bded;
+    width: 111px;
+    border-radius: 5px;
+    color: #09bded !important;
+  }
+}
+
+.discover {
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+
+  a {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 15px;
+
+    img {
+      margin-right: 16px;
+    }
+  }
 }
 </style>
