ac13ed6f0a982c0fc084bb3929b38c6598d9a724
«Diarit-S <diarit-94@hotmail.fr>
2020-06-23 16:57:39 +0200

    :wrench: [config]: update pwa configs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   lintOnSave: false,
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "SIVIK",
+    themeColor: "#430000",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: {
+      display: "fullscreen"
+    },
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "src/service-worker.js"
+    }
+  }
 };
