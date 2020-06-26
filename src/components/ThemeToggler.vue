4c539eb6ebdb5f26747ed83263f1c5900af2928a
«Diarit-S <diarit-94@hotmail.fr>
2020-06-26 12:01:54 +0200

    🚧 [feature] : finish styling theme toggler

--- src/components/ThemeToggler.vue
+++ src/components/ThemeToggler.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   div.theme-toggler
-    v-switch(v-model="$vuetify.theme.dark" label="Theme")
+    v-switch(
+      v-model="$vuetify.theme.dark" 
+      label="Theme"
+      inset
+      :ripple="false"
+    )
 </template>
 
 <script>
@@ -21,5 +26,45 @@ export default {
   position: absolute;
   right: 10px;
   top: 10px;
+}
+
+.v-input--switch__thumb {
+  background-size: 65%;
+  background-position: center;
+  border: 3px solid;
+}
+
+.v-input--switch__thumb.theme--light {
+  background-image: url("../assets/icons/light_toggler_thumb.svg");
+  height: 28px;
+  width: 28px;
+  top: calc(50% - 14px);
+  border-color: #60daff;
+}
+
+.v-input--switch__thumb.theme--dark {
+  background-image: url("../assets/icons/dark_toggler_thumb.svg");
+  height: 28px;
+  width: 28px;
+  top: calc(50% - 14px);
+  border-color: #3e4eb7;
+}
+
+.v-input--switch__track {
+  background-size: cover;
+}
+
+.v-input--switch--inset .v-input--switch__track.theme--light {
+  transform: translateX(4px);
+  background-color: #60daff;
+  opacity: 1;
+  background-image: url("../assets/icons/light_toggler_track.svg");
+}
+
+.v-input--switch--inset .v-input--switch__track.theme--dark {
+  transform: translateX(4px);
+  background-color: #3e4eb7;
+  opacity: 1;
+  background-image: url("../assets/icons/dark_toggler_track.svg");
 }
 </style>
