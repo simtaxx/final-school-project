8a6804dd83a5f6a07e3f72d6ea2a42e8e0f1125f
«Diarit-S <diarit-94@hotmail.fr>
2020-06-27 12:22:10 +0200

    🚧 [feature] : work on articles road structure

--- src/views/HomePage/components/ArticleSticker.vue
+++ src/views/HomePage/components/ArticleSticker.vue
@@ -1,40 +1,78 @@
 <template lang="pug">
-  div(:class="$style.articleSticker")
+  div(:class="[$style.articleSticker, $style[className]]")
     v-avatar(
       :class="$style.avatar"
     )
       v-icon(v-text="article.icon")
-    span {{ article.articleName }}
-    article-branches
+    span(:class="{[$style.stickerText] : className !== 'single'}") {{ article.articleName }}
+    v-btn fdds
 </template>
 
 <script>
-import ArticleBranches from "./ArticleBranches.vue"
-
 export default {
   name: "ArticleSticker",
-  components: {
-    ArticleBranches
-  },
   props: {
     article: {
       type: Object,
       required: true
+    },
+    className: {
+      type: String,
+      required: false
     }
   }
 }
-
-// On two left right
 </script>
 
 <style lang="scss" module>
+@import "~vuetify/src/styles/styles.sass";
+
 .articleSticker {
   display: flex;
   flex-direction: column;
   align-items: center;
 
+  z-index: 2;
+
   .avatar {
     background-color: gray;
   }
+}
+
+.sec1 {
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+
+.sec2 {
+  left: 100%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+
+.sec3 {
+  left: 50%;
+  top: 100%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+
+.sec4 {
+  top: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+
+.stickerText {
+  position: absolute;
+  white-space: nowrap;
+  top: 100%;
+  background-color: map-get($material-dark, "background");
+}
+
+.single {
+  margin: 6rem 0 2rem 0;
 }
 </style>
--- src/views/HomePage/components/ChapterRoad.vue
+++ src/views/HomePage/components/ChapterRoad.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
   section(:class="$style.chapterRoad")
     chapter-head(:chapterHead="chapterHead")
+    template(v-if="structuredChapterRoad.cube")
+      div(:class="[$style.cube, $style.outer]")
+        div(:class="[$style.cube, $style.inner]")
+        article-sticker(
+          v-for="(article, index) in structuredChapterRoad.cube" 
+          :article="article"
+          :className="'sec' + (index + 1)" 
+          :key="chapter.categoryName + 'cubeArticle' + index"
+        )
     article-sticker(
-      v-for="(article, index) in chapter.articles" 
-      :article="article" 
-      :key="chapter.categoryName + 'article' + index"
+      v-for="(article, index) in structuredChapterRoad.single" 
+      :article="article"
+      className="single"
+      :key="chapter.categoryName + 'singleArticle' + index"
     )
 </template>
 
@@ -31,6 +41,21 @@ export default {
         title: chapter.categoryName,
         icon: chapter.icon
       }
+    },
+    structuredChapterRoad() {
+      const articles = this.chapter.articles
+      return this.groupBy(articles, "branchesModel")
+    },
+    vueety() {
+      return this.$vuetify
+    }
+  },
+  methods: {
+    groupBy(articles, key) {
+      return articles.reduce((acc, article) => {
+        ;(acc[article[key]] = acc[article[key]] || []).push(article)
+        return acc
+      }, {})
     }
   }
 }
@@ -41,5 +66,51 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.outer {
+  width: 400px;
+  height: 400px;
+  position: relative;
+}
+
+.cube:before,
+.cube:after {
+  content: "";
+  position: absolute;
+  height: 50%;
+  width: 50%;
+}
+
+.outer:before {
+  //- Top left corner
+  border-left: 3px solid green;
+  border-top: 3px solid green;
+  border-radius: 100px 0px 0px 0px;
+}
+
+.outer:after {
+  //- Top Right corner
+  right: 0;
+  border-right: 3px solid red;
+  border-top: 3px solid red;
+  border-radius: 0px 100px 0px 0px;
+}
+
+.inner:before {
+  //- Bottom Left corner
+  bottom: 0;
+  border-left: 3px solid black;
+  border-bottom: 3px solid black;
+  border-radius: 0px 0px 0px 100px;
+}
+
+.inner:after {
+  //- Bottom Right corner
+  bottom: 0;
+  right: 0;
+  border-right: 3px solid blue;
+  border-bottom: 3px solid blue;
+  border-radius: 0px 0px 100px 0px;
 }
 </style>
