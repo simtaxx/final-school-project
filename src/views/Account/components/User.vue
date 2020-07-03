298277e8c626415194b48833bb1c74b0c24cd3bf
simtaxx <souici.haris@gmail.com>
2020-07-03 15:54:49 +0200

    feat(profil): did the profil page

---
+++ src/views/Account/components/User.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  div(:class="$style.user")
+    img(src="/img/icons/profil-picture.png")
+    div
+      h3 Cassandra Doux
+      p Cassdoux
+</template>
+
+<script>
+export default {
+  name: "User"
+}
+</script>
+
+<style lang="scss" module>
+@import "@/scss/core/colors.scss";
+.user {
+  margin: 80px 0 55px;
+  display: flex;
+  align-items: center;
+  font-family: "Gotham", sans-serif;
+
+  img {
+    width: 68px;
+    height: 68px;
+    margin-right: 16px;
+  }
+  h3 {
+    margin-bottom: 0 !important;
+    font-size: 34px;
+    color: var(--v-text-base);
+  }
+  p {
+    margin: 0;
+    color: $greyDarken;
+  }
+}
+</style>
