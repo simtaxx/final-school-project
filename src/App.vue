b0b8d9220bf038028f98ab1be5b4683768ef650a
simtaxx <souici.haris@gmail.com>
2020-07-03 11:51:42 +0200

    fix(padding app): delete app padding

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       v-col(:cols="$vuetify.breakpoint.xs ? 0 : 3" v-if="$vuetify.breakpoint.smAndUp")
         Navigation(v-if="!isFirstTime")
       v-col(:cols="$vuetify.breakpoint.xs ? 12 : 9")
-        router-view(v-if="!isFirstTime" :class="$style.app")
+        router-view(v-if="!isFirstTime")
     theme-toggler
     Introduction(v-if="isFirstTime" @closeIntroduction="isFirstTime = false")
     MobileNavigation(v-if="!isFirstTime && $vuetify.breakpoint.xs")
@@ -37,7 +37,4 @@ export default {
 
 <style lang="scss" module>
 @import "@/scss/core/colors.scss";
-.app {
-  padding-left: 290px;
-}
 </style>
